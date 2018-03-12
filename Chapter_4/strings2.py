4d214d5eebe2829c5e88fd877bc11299d7d7a538
Amar Prasovic <amar.prasovic@gmail.com>
2018-03-12 19:51:18 +0100

    learning string methods

--- Chapter_4/strings2.py
+++ Chapter_4/strings2.py
@@ -3,4 +3,35 @@ Created on 12 Mar 2018
 
 @author: amar.prasovic
 '''
-print("test")
+
+S = 'Spam'
+
+S.find('pa')
+
+print(S)
+
+S.replace('pa', 'XYZ')
+
+print(S)
+
+line = 'aaa,bbb,ccccc,dd'
+print(line.split(','))
+
+print(line)
+
+print(S.upper())
+
+print(S.isalpha())
+
+print(S.isdigit())
+
+line2 = 'aaa,bbb,ccccc,dd\n'
+
+print(line2)
+
+print(line.rstrip())
+
+print(line.rstrip().split(','))
+
+
+
