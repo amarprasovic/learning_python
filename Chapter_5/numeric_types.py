5e69bc8c1f6b98f2e1f1b6ab6fdc9e61213d2cc5
Amar Prasovic <amar.prasovic@gmail.com>
2018-03-15 16:17:07 +0100

    doin' numbers

---
+++ Chapter_5/numeric_types.py
@@ -0,0 +1,32 @@
+'''
+Created on 15 Mar 2018
+
+@author: amar.prasovic
+'''
+import math
+a = 3
+b = 4
+
+print(a+1, a-1)
+print(b*3, b/2)
+print(a%2, b**2)
+print(2+4.0, 2.0**b)
+
+print(b/2+a)
+print(b/(2.0+a))
+
+X = 2
+Y = 4
+Z = 6
+
+print(a/b)
+print(a//b)
+
+print(math.floor(2.5))
+print(math.floor(-2.5))
+
+print(math.trunc(2.5))
+print(math.trunc(-2.5))
+
+
+
